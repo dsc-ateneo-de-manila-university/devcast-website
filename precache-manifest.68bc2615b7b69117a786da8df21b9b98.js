d1bbc93f92d8c9f58f40323ced3d98a5796f5984
Harvey Sison <harveyjaysison@gmail.com>
2020-10-19 22:24:14 +0800

    Initial dist subtree commit

---
+++ precache-manifest.68bc2615b7b69117a786da8df21b9b98.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0194b74c0364820dab97",
+    "url": "/devcast-website/css/app.fa05fdee.css"
+  },
+  {
+    "revision": "3a470dfba6bfa14b1672",
+    "url": "/devcast-website/css/chunk-vendors.27cc3cd2.css"
+  },
+  {
+    "revision": "34cf3b53689abf9b72cfbb57c4f04f96",
+    "url": "/devcast-website/fonts/googlesans-medium.34cf3b53.woff"
+  },
+  {
+    "revision": "e0531fa74246e39c88c289778705726a",
+    "url": "/devcast-website/fonts/googlesans-regular.e0531fa7.woff"
+  },
+  {
+    "revision": "34aa0f5cc1c894811d483cfe3e77c483",
+    "url": "/devcast-website/img/illustration.34aa0f5c.png"
+  },
+  {
+    "revision": "54b11495e511e31fd24346efad62e975",
+    "url": "/devcast-website/img/logo.54b11495.svg"
+  },
+  {
+    "revision": "8622056ae94aae4b5dbf0ca7c7c75f4c",
+    "url": "/devcast-website/img/logo.8622056a.png"
+  },
+  {
+    "revision": "0a3de5cda49408629125d128885bc139",
+    "url": "/devcast-website/img/one.0a3de5cd.png"
+  },
+  {
+    "revision": "4651647aa62e621969c26886fa6422d9",
+    "url": "/devcast-website/img/one.4651647a.png"
+  },
+  {
+    "revision": "7fc43a1ce918ccfb65cfeee75e64ee36",
+    "url": "/devcast-website/index.html"
+  },
+  {
+    "revision": "0194b74c0364820dab97",
+    "url": "/devcast-website/js/app.bf226ecd.js"
+  },
+  {
+    "revision": "3a470dfba6bfa14b1672",
+    "url": "/devcast-website/js/chunk-vendors.51d2528e.js"
+  },
+  {
+    "revision": "2b13402ad14ab345c823b73fccec0b27",
+    "url": "/devcast-website/manifest.json"
+  },
+  {
+    "revision": "1ad32607483c6a3a4724c73af1e4324e",
+    "url": "/devcast-website/media/episodeOne.1ad32607.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/devcast-website/robots.txt"
+  },
+  {
+    "revision": "1ad32607483c6a3a4724c73af1e4324e",
+    "url": "/devcast-website/sounds/seasonOne/episodeOne.mp3"
+  }
+]);
